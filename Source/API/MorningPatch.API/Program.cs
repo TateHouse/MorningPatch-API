2feebc1082ba74cbcf2b2a8acf0909c797f528c9
TateHouse <tatemonroehouse@gmail.com>
2024-09-16 12:46:15 -0400

    Feature: API entry point

--- Source/API/MorningPatch.API/Program.cs
+++ Source/API/MorningPatch.API/Program.cs
@@ -1,8 +1,13 @@
 namespace MorningPatch.API;
 public class Program
 {
-	public static void Main(string[] args)
+	public async static Task Main(string[] args)
 	{
+		var webApplicationBuilder = WebApplication.CreateBuilder();
+		webApplicationBuilder.ConfigureServices();
 
+		var application = webApplicationBuilder.Build();
+		application.ConfigureMiddleware();
+		await application.RunAsync();
 	}
 }
