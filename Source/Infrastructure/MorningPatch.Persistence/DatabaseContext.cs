ebdf2ea30c21ca155950eb5895eb79f24b6aafba
TateHouse <tatemonroehouse@gmail.com>
2024-09-17 20:45:26 -0400

    Feature: Database context

---
+++ Source/Infrastructure/MorningPatch.Persistence/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿namespace MorningPatch.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+/**
+ * <summary>
+ * The application's Entity Framework Core <see cref="DbContext"/>.
+ * </summary>
+ */
+public sealed class DatabaseContext : DbContext
+{
+	/**
+	 * <summary>
+	 * Instantiates a new <see cref="DatabaseContext"/> instance.
+	 * </summary>
+	 * <param name="options">The options to be used by the <see cref="DatabaseContext"/>.</param>
+	 */
+	public DatabaseContext(DbContextOptions<DatabaseContext> options)
+		: base(options)
+	{
+
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+	}
+}
